43037b0f65c0aa13c80b7ec86fbbf6077a7f11e8
Niel Benjamin Kho <benjokho@gmail.com>
2022-07-12 15:18:40 +0800

    Add custom theme data

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+      extend: {
+          colors: {
+              'fem': {
+                  100: 'hsl(185, 75%, 39%)',
+                  200: 'hsl(229, 23%, 23%)',
+                  300: 'hsl(227, 10%, 46%)',
+                  400: 'hsl(0, 0%, 59%)',
+              }
+          },
+          backgroundImage: {
+            'bg-card': "url('/src/images/bg-pattern-top.svg')",
+          },
+          fontFamily: {
+              'kumbh-sans': ['Kumbh Sans', 'sans-serif'],
+          },
+          screens: {
+          },
+      },
   },
   plugins: [],
 }
